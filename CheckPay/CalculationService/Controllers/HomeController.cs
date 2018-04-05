dcfae51c963ba6cc9617cac68d919a6397dcc324
sarahc123 <g00330821@gmit.ie>
2018-04-05 11:03:44 +0100

    service

---
+++ CheckPay/CalculationService/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CalculationService.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index(String Gross, String Net)
+        {
+            ViewBag.Gross = "Gross =  " + Gross;
+            ViewBag.Net = "Net = " + Net;
+
+            return View();
+        }
+    }
+}
---
+++ CheckPay/CalculationService/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CalculationService.MvcApplication" Language="C#" %>
